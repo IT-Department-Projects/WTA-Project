5413d860b6eebbb3dfceda878fe288641da4fac2
aniket68308 <aniket68308@gmail.com>
2017-11-12 20:24:23 +0530

    updated

---
+++ PYTHON-BACKEND/youtube_scrap.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 12 18:24:49 2017
+
+@author: Aniket
+"""
+
+import bs4,requests
+query = "query"
+UrL = "https://www.youtube.com/results?search_query="+query
+#UrL = "https://www.youtube.com/results?search_query=Joe+Sent+Me+Vanessa+Daou+Joe+Sent+Me"
+
+handle = requests.get(UrL).content
+
+#print handle
+soup = bs4.BeautifulSoup(handle,"lxml")
+selector = "div.yt-lockup-dismissable.yt-uix-tile > div.yt-lockup-thumbnail.contains-addto > a"
+elems = soup.select(selector)
+helper = "https://www.youtube.com/embed" + elems[0].get('href')
+print helper
